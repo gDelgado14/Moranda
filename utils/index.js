7b01082c5d90eb93d30a4acd6c99a4134400a14b
Giorgio Delgado <delg4200@mylaurier.ca>
2016-07-19 17:30:26 -0400

    changed auth upgrade msg to Slack attachment

--- utils/index.js
+++ utils/index.js
@@ -19,8 +19,19 @@ Utils.addNewScopes = function addNewScopes (bot, slackMessageObj) {
     // getAuthorizeURL provies a url with the initial scopes
     // we then replace the initial scopes with the upgraded scopes
     let url = bot.botkit.getAuthorizeURL(slackMessageObj.team_id).replace(/&scope=(\w+(,\w+)*)/gi, '&scope=' + scopes.upgradedScopes)
-    let msg = `Hey, you don\'t have the required authentication permissions to launch Sessions.\nplease authorize with the following link:\n${url}\nTry your command once more after you have authorized.`
-
+    // let msg = `Hey, you don\'t have the required authentication permissions to launch Sessions.\nplease authorize with the following link:\n${url}\nTry your command once more after you have authorized.`
+    let msg = {
+        "attachments": [
+            {
+                "fallback": "Upgrade scopes.",
+                "color": "#36a64f",
+                "pretext": "Hey, you don't have the required authentication permissions to launch Sessions.\nplease authorize with the following link:\nTry your command once more after you have authorized.",
+                "title": "Upgrade Permissions",
+                "title_link": url,
+                "text": "Please click the above link to upgrade authentication"
+            }
+        ]
+    }
     bot.replyPrivate(slackMessageObj, msg)
 }
 
